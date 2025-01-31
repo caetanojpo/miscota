74e7b19788682726a6a7f86d58b8dcf1c5c61506
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-01-30 21:28:36 -0300

    scale page

--- src/app/balanca/page.tsx
+++ src/app/balanca/page.tsx
@@ -1,5 +1,32 @@
+"use client";
+
+import { Flex } from "@chakra-ui/react";
 import React from "react";
+import ScaleIntroduction from "@/components/molecules/section/scaleIntroduction";
+import ScaleForms from "@/components/molecules/forms/scaleForms";
 
 export default function Scale() {
-  return <div>Scale</div>;
+  return (
+    <Flex
+      bg="light"
+      h="100%"
+      w="100%"
+      position="relative"
+      direction="column"
+      gap={{ xl: "10" }}
+    >
+      <ScaleIntroduction />
+      <Flex
+        direction="column"
+        w="100%"
+        h="100%"
+        align="center"
+        gap={{ base: "10", xl: "14" }}
+        mb={{ base: 20, md: 20, xl: 40 }}
+        justify="center"
+      >
+        <ScaleForms />
+      </Flex>
+    </Flex>
+  );
 }
