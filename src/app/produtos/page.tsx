c056c4776ece3adb6376c96a50918c069232563c
tyoutecnologia <167229791+tyoutecnologia@users.noreply.github.com>
2025-02-01 15:24:53 -0300

    add layout final home

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,27 +1,6 @@
 "use client";
 import React from "react";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Products() {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        backgroundColor: "white",
-        color: "red",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <h3 style={{ fontSize: "40px", fontWeight: "bold" }}>Oiii Linda</h3>
-      <DotLottieReact
-        src="https://lottie.host/eba9dee1-6388-4357-8331-ccc1b4e2808e/m81D9Rx0tR.lottie"
-        loop
-        autoplay
-        style={{ width: "300px", height: "300px" }}
-      />
-    </div>
-  );
+  return <></>;
 }
