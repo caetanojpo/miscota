f483d925c23a1c23f44c18f882d8592fde26aac0
tyoutecnologia <167229791+tyoutecnologia@users.noreply.github.com>
2025-02-02 21:21:12 -0300

    commited product page

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,6 +1,181 @@
 "use client";
+import { TextDecoration } from "@/components/atoms/textDecoration";
+import CardContainer from "@/components/organisms/cardContainer";
+import CardContainerCookie from "@/components/organisms/cardContainerCookie";
+import { Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
 export default function Products() {
-  return <></>;
+  return (
+    <Flex bgColor="#f5f2e9" flexDir="column">
+      <Flex position="relative">
+        <Image
+          alt=""
+          src="/bone.png"
+          width="129px"
+          transform="translate(200px, 20px) rotate(-15deg)"
+          height="77px"
+          zIndex={999}
+        />
+        <TextDecoration
+          text="Nossos"
+          position="absolute"
+          maxW="350px"
+          top="-100px"
+          left="-500px"
+          textAlign="center"
+          textStrokeColor2="#f5f2e9"
+          fontSize="80px"
+        />
+        <TextDecoration
+          text="Produtos"
+          position="absolute"
+          transform="rotate(-5deg)"
+          maxW="500px"
+          top="-50px"
+          left="-400px"
+          textAlign="center"
+          textStrokeColor1="#f7c657"
+          textStrokeColor2="#f5f2e9"
+          fontSize="80px"
+        />
+      </Flex>
+      <Flex
+        w="100%"
+        justify="center"
+        align="center"
+        mt="200px"
+        mb="200px"
+        flexDir="column"
+        position="relative"
+      >
+        <Image
+          alt=""
+          src="/paw.png"
+          w="100px"
+          position="absolute"
+          left="75%"
+          top="100px"
+          transform="rotate(20deg)"
+        />
+        <Image
+          alt=""
+          src="/bone.png"
+          w="160px"
+          top="400px"
+          left="26%"
+          position="absolute"
+          transform="rotate(20deg)"
+        />
+        <Text
+          color="#893733"
+          maxW="70%"
+          fontSize="30px"
+          textAlign="center"
+          fontWeight="bold"
+        >
+          Você ja ouviu aquela frase que diz que nós somos aquilo que comemos?
+          Com os nossos pets são a mesma coisa, a alimentação saudável é
+          essencial para a longevidade e qualidade de vida dos nossos bichinhos!
+        </Text>
+        <Text
+          color="#893733"
+          maxW="70%"
+          fontSize="30px"
+          textAlign="center"
+          mt="100px"
+          fontWeight="bold"
+        >
+          A maior missão da Miscota é trazer para todos de forma prática e
+          acessivel uma forma de levar o alimento mais puro e fresco para o seu
+          pet, trazendo não só a nutrição mas tambem saciedade e alegria para
+          ele. Confira abaixo os nossos produtinhos.
+        </Text>
+      </Flex>
+      <Flex
+        borderTop="4px solid #f7c657"
+        w="100%"
+        h="900px"
+        mb="70px"
+        position="relative"
+      >
+        <TextDecoration
+          text="Alimentação"
+          position="absolute"
+          maxW="500px"
+          top="-20"
+          textAlign="center"
+          textStrokeColor1="#893733"
+          textStrokeColor2="#f5f2e9"
+          fontSize="70px"
+        />
+        <TextDecoration
+          text="Natural"
+          position="absolute"
+          maxW="500px"
+          top="-5"
+          left="100px"
+          textAlign="center"
+          textStrokeColor1="#f7c757"
+          textStrokeColor2="#f5f2e9"
+          fontSize="70px"
+        />
+        <Image
+          alt=""
+          src="/pote.png"
+          width="160px"
+          h="100px"
+          filter="drop-shadow(0px 0px 13px black)"
+          position="absolute"
+          left="700px"
+          top="10px"
+        />
+        <Flex w="100%">
+          <CardContainer />
+        </Flex>
+      </Flex>
+      <Flex
+        borderTop="4px solid #f7c657"
+        w="100%"
+        h="900px"
+        mb="70px"
+        position="relative"
+      >
+        <TextDecoration
+          text="Biscoitos"
+          position="absolute"
+          maxW="500px"
+          top="-90px"
+          textAlign="center"
+          textStrokeColor1="#893733"
+          textStrokeColor2="#f5f2e9"
+          fontSize="70px"
+        />
+        <TextDecoration
+          text="Pet"
+          position="absolute"
+          maxW="500px"
+          top="-5"
+          left="100px"
+          textAlign="center"
+          textStrokeColor1="#f7c757"
+          textStrokeColor2="#f5f2e9"
+          fontSize="70px"
+        />
+        <Image
+          alt=""
+          src="/bone.png"
+          width="165px"
+          h="100px"
+          transform="rotate(9deg)"
+          position="absolute"
+          left="780px"
+          top="10px"
+        />
+        <Flex w="100%">
+          <CardContainerCookie />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
---
+++ src/components/organisms/cardContainerCookie/index.tsx
@@ -0,0 +1,297 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+import "./style.css";
+import { ButtonPayment } from "@/components/molecules/button";
+
+const Card = () => {
+  return (
+    <div className="body">
+      <div className="container">
+        <div className="card">
+          <div
+            className="face face1"
+            style={{
+              height: "450px",
+              width: "350px",
+              position: "relative",
+            }}
+          >
+            <Image
+              alt=""
+              src="/seal-5.png"
+              w="90px"
+              h="90px"
+              position="absolute"
+              top="-30px"
+              left="290px"
+            />
+            <Image
+              alt=""
+              src="/gluten-free-blue.png"
+              w="60px"
+              position="absolute"
+              top="2"
+              left="10px"
+            />
+            <Image
+              alt=""
+              src="/100-natural-blue.png"
+              w="60px"
+              position="absolute"
+              top="2"
+              left="80px"
+            />
+            <Flex position="absolute" left="270px" flexDir="column" top="20">
+              <Text color="#024f6c" textAlign="center">
+                Peso liquido
+              </Text>
+              <Text
+                color="#024f6c"
+                fontWeight="bold"
+                textAlign="center"
+                fontSize="20px"
+              >
+                100g
+              </Text>
+            </Flex>
+            <Flex position="absolute">
+              <Image alt="" src="/calm-dog.png" w="240px" />
+            </Flex>
+            <Flex
+              flexDirection="column"
+              justify="center"
+              alignItems="center"
+              w="100%"
+              position="absolute"
+              top="380px"
+            >
+              <Text fontSize="35px" fontWeight="bold" color="#024f6c">
+                Calm Dog
+              </Text>
+              <Text
+                fontSize="20px"
+                color="#f5bb15"
+                textAlign="center"
+                fontWeight="bold"
+                maxWidth="200px"
+              >
+                Spitulina azul, Maracujá e Frango.
+              </Text>
+            </Flex>
+          </div>
+          <div
+            className="face face2"
+            style={{ height: "300px", width: "350px" }}
+          >
+            <div
+              className="content"
+              style={{
+                width: "100% !important",
+                flexDirection: "column",
+                padding: "10px",
+              }}
+            >
+              <Flex px="20px">
+                <Text color="#024f6c" textAlign="center" fontWeight="bold">
+                  Farinha de arroz, frango, Maracujá, Erva cidreira, Erva doce,
+                  Camomila, Lavanda, Banha suina, ovo, Spirulina azul
+                </Text>
+              </Flex>
+              <Flex w="100%" justifyContent="center" mt="30px">
+                <ButtonPayment />
+              </Flex>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div
+            className="face face1"
+            style={{
+              height: "450px",
+              width: "350px",
+              position: "relative",
+            }}
+          >
+            <Image
+              alt=""
+              src="/seal-6.png"
+              w="90px"
+              h="90px"
+              position="absolute"
+              top="-30px"
+              left="290px"
+            />
+            <Image
+              alt=""
+              src="/gluten-free-brown.png"
+              w="60px"
+              position="absolute"
+              top="2"
+              left="10px"
+            />
+            <Image
+              alt=""
+              src="/100-natural-brown.png"
+              w="60px"
+              position="absolute"
+              top="2"
+              left="80px"
+            />
+            <Flex position="absolute" left="270px" flexDir="column" top="20">
+              <Text color="#95584a" textAlign="center">
+                Peso liquido
+              </Text>
+              <Text
+                color="#95584a"
+                fontWeight="bold"
+                textAlign="center"
+                fontSize="20px"
+              >
+                100g
+              </Text>
+            </Flex>
+            <Flex position="absolute">
+              <Image alt="" src="/cookie-frutas.png" w="240px" />
+            </Flex>
+            <Flex
+              flexDirection="column"
+              justify="center"
+              alignItems="center"
+              w="100%"
+              position="absolute"
+              top="380px"
+            >
+              <Text fontSize="35px" fontWeight="bold" color="#95584a">
+                Frutinhas
+              </Text>
+              <Text
+                fontSize="20px"
+                color="#e2bb97"
+                textAlign="center"
+                fontWeight="bold"
+                maxWidth="200px"
+              >
+                Frutas vermelhas, Banana e Mel
+              </Text>
+            </Flex>
+          </div>
+          <div
+            className="face face2"
+            style={{ height: "300px", width: "350px" }}
+          >
+            <div
+              className="content"
+              style={{
+                width: "100% !important",
+                flexDirection: "column",
+                padding: "10px",
+              }}
+            ></div>
+          </div>
+        </div>
+        <div className="card">
+          <div
+            className="face face1"
+            style={{
+              height: "450px",
+              width: "350px",
+              position: "relative",
+            }}
+          >
+            <Image
+              alt=""
+              src="/seal-7.png"
+              w="90px"
+              h="90px"
+              position="absolute"
+              top="-30px"
+              left="290px"
+            />
+            <Image
+              alt=""
+              src="/gluten-free-green.png"
+              w="60px"
+              position="absolute"
+              top="2"
+              left="10px"
+            />
+            <Image
+              alt=""
+              src="/100-natural-green.png"
+              w="60px"
+              position="absolute"
+              top="2"
+              left="80px"
+            />
+            <Flex position="absolute" left="270px" flexDir="column" top="20">
+              <Text color="#989c38" textAlign="center">
+                Peso liquido
+              </Text>
+              <Text
+                color="#989c38"
+                fontWeight="bold"
+                textAlign="center"
+                fontSize="20px"
+              >
+                100g
+              </Text>
+            </Flex>
+            <Flex position="absolute">
+              <Image alt="" src="/dental-fresh.png" w="240px" />
+            </Flex>
+            <Flex
+              flexDirection="column"
+              justify="center"
+              alignItems="center"
+              w="100%"
+              position="absolute"
+              top="380px"
+            >
+              <Text fontSize="25px" fontWeight="bold" color="#989c38">
+                Dental fresh
+              </Text>
+              <Text
+                fontSize="20px"
+                color="#e2bb97"
+                textAlign="center"
+                fontWeight="bold"
+                maxWidth="200px"
+              >
+                Spitulina azul, Maracujá e Frango.
+              </Text>
+            </Flex>
+          </div>
+          <div
+            className="face face2"
+            style={{ height: "300px", width: "350px" }}
+          >
+            <div
+              className="content"
+              style={{
+                width: "100% !important",
+                flexDirection: "column",
+                padding: "10px",
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CardContainerCookie: React.FC = () => {
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      minH="100vh"
+      w="100%"
+      bg="#f5f2e9"
+      gap={6}
+    >
+      <Card />
+    </Flex>
+  );
+};
+
+export default CardContainerCookie;
